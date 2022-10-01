f86e92e3571436a0cf0a07d65478c2c803c17ea2
tnathu-ai <ngocanhthu20102002@gmail.com>
2022-10-01 21:55:19 +1000

    added feature representation

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,35 @@
 import os
 import numpy as np
-def loadGloVe(fPath):
-    path_to_glove_file = os.path.join(fPath)
+import pandas as pd
 
-    preTGloVe_wv = {}
-    with open(path_to_glove_file, encoding="utf-8") as f:
-        for line in f:
-            word, coefs = line.split(maxsplit=1)
-            coefs = np.fromstring(coefs, "f", sep=" ")
-            preTGloVe_wv[word] = coefs
+def stats_print(tk_description):
+    words = list(chain.from_iterable(tk_description)) # we put all the tokens in the corpus in a single list
+    vocab = set(words) # compute the vocabulary by converting the list of words/tokens to a set, i.e., giving a set of unique words
+    lexical_diversity = len(vocab)/len(words)
+    print("Vocabulary size: ",len(vocab))
+    print("Total number of tokens: ", len(words))
+    print("Lexical diversity: ", lexical_diversity)
+    print("Total number of description:", len(tk_description))
+    lens = [len(article) for article in tk_description]
+    print("Average description length:", np.mean(lens))
+    print("Maximum description length:", np.max(lens))
+    print("Minimum description length:", np.min(lens))
+    print("Standard deviation of description length:", np.std(lens))
 
-    print("Found %s word vectors." % len(preTGloVe_wv))
-    return preTGloVe_wv
+def gen_docVecs(wv,tk_txts): # generate vector representation for documents
+    docs_vectors = pd.DataFrame() # creating empty final dataframe
+    #stopwords = nltk.corpus.stopwords.words('english') # if we haven't pre-processed the articles, it's a good idea to remove stop words
+
+    for i in range(0,len(tk_txts)):
+        tokens = tk_txts[i]
+        temp = pd.DataFrame()  # creating a temporary dataframe(store value for 1st doc & for 2nd doc remove the details of 1st & proced through 2nd and so on..)
+        for w_ind in range(0, len(tokens)): # looping through each word of a single document and spliting through space
+            try:
+                word = tokens[w_ind]
+                word_vec = wv[word] # if word is present in embeddings(goole provides weights associate with words(300)) then proceed
+                temp = pd.concat([temp, pd.Series(word_vec)], ignore_index = True)
+            except:
+                pass
+        doc_vector = temp.sum() # take the sum of each column
+        docs_vectors = pd.concat([docs_vectors, doc_vector], ignore_index = True)
+    return docs_vectors
