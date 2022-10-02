7b1b2af6b09fa45ee131e90998c9fc32d2090ab3
tnathu-ai <ngocanhthu20102002@gmail.com>
2022-10-02 12:57:05 +1100

    added weight vector

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,18 @@ import os
 import numpy as np
 import pandas as pd
 from itertools import chain
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ignore warning
+import warnings
+warnings.filterwarnings('ignore')
+
+%matplotlib inline
+# set desired matplotlib gloabal figure size
+plt.rcParams["figure.figsize"] = (20,10)
+
 
 # text descriptive statistic
 def stats_print(tk_description):
@@ -64,3 +76,92 @@ def gen_docVecs_weight(wv,tk_txts,tfidf = []): # generate vector representation 
         doc_vector = temp.sum() # take the sum of each column(w0, w1, w2,........w300)
         docs_vectors = pd.concat([docs_vectors, doc_vector], ignore_index = True) # append each document value to the final dataframe
     return docs_vectors
+
+
+
+def plotTSNE(labels,features): # features as a numpy array, each element of the array is the document embedding of an article
+    categories = sorted(labels.unique())
+    # Sampling a subset of our dataset because t-SNE is computationally expensive
+    SAMPLE_SIZE = int(len(features) * 0.3)
+    np.random.seed(0)
+    indices = np.random.choice(range(len(features)), size=SAMPLE_SIZE, replace=False)
+    projected_features = TSNE(n_components=2, random_state=0).fit_transform(features[indices])
+    colors = ['pink', 'green', 'midnightblue', 'orange', 'darkgrey']
+    for i in range(0,len(categories)):
+        points = projected_features[(labels[indices] == categories[i])]
+        plt.scatter(points[:, 0], points[:, 1], s=30, c=colors[i], label=categories[i])
+    plt.title("Feature vector for each article, projected on 2 dimensions.",
+              fontdict=dict(fontsize=15))
+    plt.legend()
+    plt.show()
+    
+    
+    
+    
+# tsne plot for below word
+# source: https://thinkinfi.com/fasttext-word-embeddings-python-implementation/
+def tsne_plot_word(for_word, w2v_model):
+    # trained fastText model dimention
+    dim_size = w2v_model.wv.vectors.shape[1]
+ 
+    arrays = np.empty((0, dim_size), dtype='f')
+    word_labels = [for_word]
+    color_list  = ['red']
+ 
+    # adds the vector of the query word
+    arrays = np.append(arrays, w2v_model.wv.__getitem__([for_word]), axis=0)
+ 
+    # gets list of most similar words
+    sim_words = w2v_model.wv.most_similar(for_word, topn=10)
+ 
+    # adds the vector for each of the closest words to the array
+    for wrd_score in sim_words:
+        wrd_vector = w2v_model.wv.__getitem__([wrd_score[0]])
+        word_labels.append(wrd_score[0])
+        color_list.append('pink')
+        arrays = np.append(arrays, wrd_vector, axis=0)
+ 
+    #---------------------- Apply PCA and tsne to reduce dimention --------------
+ 
+    # fit 2d PCA model to the similar word vectors
+    model_pca = PCA(n_components = 10).fit_transform(arrays)
+ 
+    # Finds 2d coordinates t-SNE
+    np.set_printoptions(suppress=True)
+    Y = TSNE(n_components=2, random_state=0, perplexity=15).fit_transform(model_pca)
+ 
+    # Sets everything up to plot
+    df_plot = pd.DataFrame({'x': [x for x in Y[:, 0]],
+                       'y': [y for y in Y[:, 1]],
+                       'words_name': word_labels,
+                       'words_color': color_list})
+ 
+    #------------------------- tsne plot Python -----------------------------------
+ 
+    # plot dots with color and position
+    plot_dot = sns.regplot(data=df_plot,
+                     x="x",
+                     y="y",
+                     fit_reg=False,
+                     marker="o",
+                     scatter_kws={'s': 40,
+                                  'facecolors': df_plot['words_color']
+                                 }
+                    )
+ 
+    # Adds annotations with color one by one with a loop
+    for line in range(0, df_plot.shape[0]):
+         plot_dot.text(df_plot["x"][line],
+                 df_plot['y'][line],
+                 '  ' + df_plot["words_name"][line].title(),
+                 horizontalalignment='left',
+                 verticalalignment='bottom', size='medium',
+                 color=df_plot['words_color'][line],
+                 weight='normal'
+                ).set_size(15)
+ 
+ 
+    plt.xlim(Y[:, 0].min()-50, Y[:, 0].max()+50)
+    plt.ylim(Y[:, 1].min()-50, Y[:, 1].max()+50)
+ 
+    plt.title('t-SNE visualization for word "{}'.format(for_word.title()) +'"')
