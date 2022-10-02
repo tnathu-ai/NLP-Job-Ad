9c743eb4bb3e3061271e94864193a1a45d435c4f
tnathu-ai <ngocanhthu20102002@gmail.com>
2022-10-02 12:20:01 +1100

    added fasttext word embeding

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from itertools import chain
 
+# text descriptive statistic
 def stats_print(tk_description):
     words = list(chain.from_iterable(tk_description)) # we put all the tokens in the corpus in a single list
     vocab = set(words) # compute the vocabulary by converting the list of words/tokens to a set, i.e., giving a set of unique words
@@ -17,7 +18,9 @@ def stats_print(tk_description):
     print("Minimum description length:", np.min(lens))
     print("Standard deviation of description length:", np.std(lens))
 
-def gen_docVecs(wv,tk_txts): # generate vector representation for documents
+    
+ # generate vector representation for documents    
+def gen_docVecs(wv,tk_txts):
     docs_vectors = pd.DataFrame() # creating empty final dataframe
     #stopwords = nltk.corpus.stopwords.words('english') # if we haven't pre-processed the articles, it's a good idea to remove stop words
 
@@ -33,4 +36,31 @@ def gen_docVecs(wv,tk_txts): # generate vector representation for documents
                 pass
         doc_vector = temp.sum() # take the sum of each column
         docs_vectors = pd.concat([docs_vectors, doc_vector], ignore_index = True)
+    return docs_vectors
+
+
+
+# extended version of the `gen_docVecs` function - generate tfidf weight document embeding
+def gen_docVecs_weight(wv,tk_txts,tfidf = []): # generate vector representation for documents
+    docs_vectors = pd.DataFrame() # creating empty final dataframe
+    #stopwords = nltk.corpus.stopwords.words('english') # removing stop words
+
+    for i in range(0,len(tk_txts)):
+        tokens = list(set(tk_txts[i])) # get the list of distinct words of the document
+
+        temp = pd.DataFrame()  # creating a temporary dataframe(store value for 1st doc & for 2nd doc remove the details of 1st & proced through 2nd and so on..)
+        for w_ind in range(0, len(tokens)): # looping through each word of a single document and spliting through space
+            try:
+                word = tokens[w_ind]
+                word_vec = wv[word] # if word is present in embeddings(goole provides weights associate with words(300)) then proceed
+                
+                if tfidf != []:
+                    word_weight = float(tfidf[i][word])
+                else:
+                    word_weight = 1
+                temp = pd.concat([temp, pd.Series(word_vec*word_weight)], ignore_index = True) # if word is present then append it to temporary dataframe
+            except:
+                pass
+        doc_vector = temp.sum() # take the sum of each column(w0, w1, w2,........w300)
+        docs_vectors = pd.concat([docs_vectors, doc_vector], ignore_index = True) # append each document value to the final dataframe
     return docs_vectors
